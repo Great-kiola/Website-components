3d0b6c62e8cabc0e035b50dfb1aa58aa8dff1429
Ola <hollabodey.kilaso@gmail.com>
2023-05-05 13:10:44 +0100

    Made new changes

--- Pricing/script.js
+++ Pricing/script.js
@@ -1,20 +1,7 @@
 let close =  document.querySelector('.btnClass');
 let Mycontainer = document.querySelector('.container');
+let toggle = document.querySelector('.toggle');
 
-close.addEventListener('click', closeContainer);
-
-function closeContainer (){
-    Mycontainer.addEventListener('click', () => {
-        Mycontainer.forEach(containers => {
-            containers.classList.add('remove');
-        })
-    });
-
-};
-
-
-
-// container.forEach(containers =>
-//     containers.addEventListener( 'click', () => {
-//         containers.classList.add('remove');
-// }));
+toggle.addEventListener('click', ()=>{
+    toggle.classList.toggle('active');
+});
